d475a50955c858a148f0fdb91a91bf52f8e37cb6
Aliaksandr Rahalevich <saksmlz@gmail.com>
2018-10-10 22:04:10 -0700

    implement auth middleware

--- src/apps/middlewares/auth_by_token.rs
+++ src/apps/middlewares/auth_by_token.rs
@@ -14,9 +14,28 @@ impl VerifyAuthToken {
         Self { secret }
     }
 
-    fn verify(&self, auth_header: &str) -> Result<Started> {
-        // TODO: verify jwt token
-        Ok(Started::Done)
+    fn verify(&self, auth_header: &str) -> Result<()> {
+        use frank_jwt::{decode, Algorithm};
+        use std::cmp::Ordering;
+        use time::now_utc;
+
+        let payload = auth_header.to_string();
+
+        decode(&payload, &self.secret, Algorithm::HS256)
+            .map_err(|_| ())
+            .and_then(|(header, _payload)| {
+                let exp = header.get("exp").ok_or(());
+                exp.and_then(|n| n.as_i64().ok_or(()))
+            })
+            .and_then(|exp| {
+                let now = now_utc().to_timespec().sec;
+
+                match exp.cmp(&now) {
+                    Ordering::Greater => Ok(()),
+                    _ => Err(()),
+                }
+            })
+            .map_err(|_| ErrorUnauthorized("TODO"))
     }
 }
 
@@ -30,6 +49,70 @@ impl<AppState> Middleware<AppState> for VerifyAuthToken {
             .to_str()
             .map_err(ErrorUnauthorized)?;
 
-        self.verify(auth_header)
+        self.verify(auth_header).map(|_| Started::Done)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::env;
+    use time::{now_utc, Duration};
+
+    fn make_token(hours_from_now: i64, secret_str: &str) -> String {
+        use frank_jwt::{encode, Algorithm};
+
+        let exp = (now_utc() + Duration::hours(hours_from_now))
+            .to_timespec()
+            .sec;
+        let header = json!({ "exp": exp });
+        let payload = json!({ "foo": 123 });
+        let secret = secret_str.to_string();
+
+        encode(header, &secret, &payload, Algorithm::HS256).expect("failed to encode token")
+    }
+
+    fn setup() {
+        env::set_var("AUTH_TOKEN_SECRET", "foo");
+    }
+
+    fn teardown() {
+        env::remove_var("AUTH_TOKEN_SECRET");
+    }
+
+    #[test]
+    fn test_verify_token() {
+        setup();
+
+        let middleware = VerifyAuthToken::new();
+        let valid_token = make_token(33, "foo");
+
+        assert!(middleware.verify(&valid_token).is_ok());
+
+        teardown()
+    }
+
+    #[test]
+    fn test_verify_expired_token() {
+        setup();
+
+        let middleware = VerifyAuthToken::new();
+        let valid_token = make_token(-33, "foo");
+
+        assert!(middleware.verify(&valid_token).is_err());
+
+        teardown();
+    }
+
+    #[test]
+    fn test_verify_token_with_wrong_signature() {
+        setup();
+
+        let middleware = VerifyAuthToken::new();
+        let valid_token = make_token(33, "bar");
+
+        assert!(middleware.verify(&valid_token).is_err());
+
+        teardown();
     }
 }
