647888f6f2ec2065bfe2819247b89fe246003857
Aliaksandr Rahalevich <saksmlz@gmail.com>
2019-04-07 20:13:18 -0700

    make force-https configurable

--- octo-budget-api/src/apps/middlewares/force_https.rs
+++ octo-budget-api/src/apps/middlewares/force_https.rs
@@ -5,48 +5,54 @@ use actix_web::{
     HttpRequest, HttpResponse, Result,
 };
 
-#[derive(Default)]
-pub struct ForceHttps; // <- Our middleware
+const HTTPS_SCHEME: &str = "https";
+const ENV_VAR_NAME: &str = "FORCE_HTTPS";
 
-/// Middleware implementation, middlewares are generic over application state,
-/// so you can access state with `HttpRequest::state()` method.
+#[derive(Default)]
+pub struct ForceHttps;
+
+impl ForceHttps {
+    fn is_enabled() -> bool {
+        std::env::var(ENV_VAR_NAME).is_ok()
+    }
+
+    fn https_url(uri: &Uri, host: &str) -> Result<Uri, actix_web::Error> {
+        let path_and_query = uri
+            .path_and_query()
+            .map(PathAndQuery::as_str)
+            .unwrap_or_else(|| "");
+
+        Uri::builder()
+            .scheme("https")
+            .authority(host)
+            .path_and_query(path_and_query)
+            .build()
+            .map_err(|e| {
+                log::error!(
+                    "Failed to generate url: {:?}, err: {:?}, path_and_query: `{}', host: `{}'",
+                    &uri,
+                    e,
+                    &path_and_query,
+                    host
+                );
+                error::ErrorUnprocessableEntity("Failed to record to HTTPS")
+            })
+    }
+}
+
 impl<S> Middleware<S> for ForceHttps {
-    /// Method is called when request is ready. It may return
-    /// future, which should resolve before next middleware get called.
     fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
         let connection_info = req.connection_info();
 
-        match connection_info.scheme() {
-            "https" => Ok(Started::Done),
-            _ => {
-                let uri = req.uri();
-                let path_and_query = uri
-                    .path_and_query()
-                    .map(PathAndQuery::as_str)
-                    .unwrap_or_else(|| "");
-
-                Uri::builder()
-                    .scheme("https")
-                    .authority(connection_info.host())
-                    .path_and_query(path_and_query)
-                    .build()
-                    .map_err(|e| {
-                        log::error!(
-                            "Failed to generate url: {:?}, err: {:?}, path_and_query: `{}', host: `{}'",
-                            &uri,
-                            e,
-                            &path_and_query,
-                            connection_info.host()
-                        );
-                        error::ErrorUnprocessableEntity("Failed to record to HTTPS")
-                    })
-                    .map(|url| {
-                        let response = HttpResponse::MovedPermanently()
-                            .header(header::LOCATION, url.to_string())
-                            .finish();
-                        Started::Response(response)
-                    })
-            }
+        if !Self::is_enabled() || HTTPS_SCHEME == connection_info.scheme() {
+            return Ok(Started::Done);
         }
+
+        Self::https_url(req.uri(), connection_info.host()).map(|url| {
+            let response = HttpResponse::MovedPermanently()
+                .header(header::LOCATION, url.to_string())
+                .finish();
+            Started::Response(response)
+        })
     }
 }
